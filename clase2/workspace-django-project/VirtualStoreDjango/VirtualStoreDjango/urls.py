d7733250e38ffd53ba902a0deea576cda942c9c5
joaonunez <joaovaldiglesias@gmail.com>
2024-08-20 11:01:35 -0400

    second app created and first app ready

--- clase2/workspace-django-project/VirtualStoreDjango/VirtualStoreDjango/urls.py
+++ clase2/workspace-django-project/VirtualStoreDjango/VirtualStoreDjango/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from firstapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('saludar', views.display_saludar),
+    path('mostrar-fecha/', views.display_date)
 ]
--- clase2/workspace-django-project/VirtualStoreDjango/firstapp/views.py
+++ clase2/workspace-django-project/VirtualStoreDjango/firstapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+import datetime
 # Create your views here.
+
+def display_saludar(request):
+    saludo = "<h1>Saludos Gente</h1>"
+    return HttpResponse(saludo)
+
+def display_date(request):
+    dt = datetime.datetime.now()
+    s = "<b>Fecha y hora:</b>: " + str(dt)
+    return HttpResponse(s)
